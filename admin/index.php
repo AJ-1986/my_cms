9c4f3b6b9a68b0e34011a8b35499776ca4f66dc5
Adam Jurewicz <mpowergcm@outlook.com>
2022-03-03 12:22:41 +0100

    Utworzenie modulu wejsciowego do logowania

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+if ($_SESSION['autoryzacja_id'] == $_SESSION['klucz_id'])
+    {
+?>
 <!DOCTYPE html>
 <html lang="pl-PL">
     <head>
@@ -6,6 +11,34 @@
         <title>Panel administracyjny Proxweb CMS</title>
     </head>
     <body>
-        
+
     </body>
 </html>
+<?php
+    }
+    else
+    {
+        session_destroy();
+?>
+<!DOCTYPE html>
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="autor" content="Adam Jurewicz">
+        <title>Panel administracyjny Proxweb CMS</title>
+        <script language="javascript" type="text/javascript">
+            function przekieruj()
+            {
+                document.location='zaloguj.php';
+            }
+        </script>
+    </head>
+    <body>
+        <script language="javascript" type="text/javascript">
+            przekieruj();
+        </script>
+    </body>
+</html>
+<?php        
+    }
+?>
