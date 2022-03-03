428a2b0427e1b39d4aad95cfba65bc2f8f8bb071
Adam Jurewicz <mpowergcm@outlook.com>
2022-03-03 11:55:03 +0100

    Utworzenie pliku administracyjnego index.php

---
+++ admin/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="autor" content="Adam Jurewicz">
+        <title>Panel administracyjny Proxweb CMS</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
