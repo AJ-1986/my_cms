d262e7b31f3f299fbfb6d601fa6aa97b7621d29f
Adam Jurewicz <mpowergcm@outlook.com>
2022-03-03 12:26:28 +0100

    Zainicjowanie modulu logowanie

---
+++ admin/zaloguj.php
@@ -0,0 +1,10 @@
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="autor" content="Adam Jurewicz">
+        <title>Panel administracyjny Proxweb CMS - logowanie</title>
+    </head>
+    <body>
+
+    </body>
+</html>
