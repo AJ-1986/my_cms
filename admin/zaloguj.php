1f19ade8ed5c5e761297c67e74cae410c0a9ddd9
Adam Jurewicz <mpowergcm@outlook.com>
2022-03-03 17:48:07 +0100

    Utworzenie wstepnego szkicu formularza logowania

--- admin/zaloguj.php
+++ admin/zaloguj.php
@@ -6,6 +6,38 @@
         <link rel="stylesheet" href="styl_zaloguj.css">
     </head>
     <body>
-        <p>To jest test</p>
+        <div class="rdzen">
+            <div class="naglowek">
+                <h1 class="tytul">Logowanie do systemu ProxWebCMS</h1>
+            </div>
+            <div class="strona_rdzen">
+                <div class="form_rdzen">
+                    <div class="form_log">
+                        <form method="post" action="log_funkcja.php">
+                            <table>
+                                <tr>
+                                    <td class="login">Nazwa użytkownika:</td>
+                                    <td><input class="login_uz" type="text" name="login"></td>
+                                </tr>
+                                <tr>
+                                    <td class="haslo">Hasło użytkownika:</td>
+                                    <td><input class="haslo_uz" type="password" name="haslo_uz"></td>
+                                </tr>
+                                <tr>
+                                    <td colspan="2" class="nawigacja">
+                                        <input class="wyczysc" type="reset" value="Wyczyść">&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;
+                                        <input class="zaloguj" type="submit" value="Zaloguj">
+                                    </td>
+                                </tr>
+                            </table>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="stopka">
+                <div class="stopka_rdzen">
+                </div>
+            </div>
+        </div>
     </body>
 </html>
