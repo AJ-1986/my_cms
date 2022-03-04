57b97127951ceed1fa8a4a940e6d602b573753d2
Adam Jurewicz <mpowergcm@outlook.com>
2022-03-04 16:15:08 +0100

    Dodanie funkcji przekierowania w przypadku blednego logowania

--- admin/log_funkcja.php
+++ admin/log_funkcja.php
@@ -6,7 +6,23 @@ if($_POST['login_uz'] == $us_login AND $_POST['haslo_uz'] == $us_paswd)
     echo 'Zalogowany';
 }
 else
-{
-    echo 'Niezalogowany';
+{    
+?>
+<!DOCTYPE html>
+<html lang="pl-PL">
+    <head>
+        <script language="javascript" type="text/javascript">
+            function przekieruj() {
+                document.location='zaloguj.php?blad=1';
+            }
+        </script>
+    </head>
+    <body>
+        <script language="javascript" type="text/javascript">
+            przekieruj();
+        </script>
+    </body>
+</html>
+<?php
 }
 ?>
