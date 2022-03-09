03cf7a0e6f506ec76a07c43f6563624b85028189
Adam Jurewicz <mpowergcm@outlook.com>
2022-03-09 16:16:09 +0100

    Utworzenie tymczasowego glownego indexu

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="autor" content="proxweb"
+        <meta name="Keywords" content="">
+        <meta name="Decription" content="">
+        <title>ProxWebCMS - strona w budowie</title>
+    </head>
+    <body>
+        <h1>Strona główna ProxwebCMS</h1>
+        <ul>
+            <li><a href="/admin/index.php">Administrator strony</a></li>
+        </ul>
+    </body>
+</html>
