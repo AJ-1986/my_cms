7eed18cf349b9baded913e7f9ed9da4bd83d9537
Adam Jurewicz <mpowergcm@outlook.com>
2022-04-01 16:57:04 +0200

    Dodanie stylu dla nagłówka 'moje_strony'

--- admin/moje_strony.php
+++ admin/moje_strony.php
@@ -1,7 +1,7 @@
 <?php
 if ($_GET['moje_strony'] == '1') {
 ?>
-                    <h1 class="tytuł_gl">Moje strony</h1>
+                    <h1 class="tytul_gl">Moje strony</h1>
                     <p>
                         <button class="sr_przyciski" onclick="otworz('moje_strony=1&nowa_str')">Dodaj nową stronę</button>
                     </p><hr>
